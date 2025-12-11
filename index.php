4e4738d10504d06fa41ff82ab009fc57eaaef996
Ahmad <Ahmad.balbissi.social@gmail.com>
2025-12-11 11:49:10 +0200

    final code

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
        echo "<h2>✅ STOP! Task Saved. Look at the data above.</h2>";
               
     }
-
 };
-
-
+     $stmt =$conn -> prepare("SELECT * FROM tasks ORDER BY created_at DESC");
+ $stmt -> execute();
+ $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
@@ -35,6 +35,22 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         <input type="text" name="title" placeholder="What needs to be done?" required>
         <button type="submit">Add Task</button>
     </form>
-
+    <h3>Current Tasks</h3>
+    <ul>
+        <?php foreach($tasks as $task): ?>
+            <li>
+                <!-- Clean the output to prevent hacking -->
+                <?= htmlspecialchars($task['title']) ?> 
+                <small>(<?= $task['created_at'] ?>)</small>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
+
+
+
+ 
+
+ 
+
